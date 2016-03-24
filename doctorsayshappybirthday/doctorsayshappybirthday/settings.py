329db1bc9a70e977351ec8b30483798e1bb142a1
cooltoast <mpatel678@gmail.com>
2016-03-23 22:06:33 -0700

    get access_token,etc. after authenticating and add html templating

--- doctorsayshappybirthday/doctorsayshappybirthday/settings.py
+++ doctorsayshappybirthday/doctorsayshappybirthday/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.utils._os import upath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -62,7 +63,8 @@ ROOT_URLCONF = 'doctorsayshappybirthday.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'happybirthday/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'happybirthday/templates'),
+                 os.path.join(os.path.dirname(upath(__file__)), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- doctorsayshappybirthday/happybirthday/views.py
+++ doctorsayshappybirthday/happybirthday/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+import datetime, pytz, requests
 
 # Create your views here.
 def login(request):
     return render(request, 'happybirthday/login.html', {'redirect_uri':settings.REDIRECT_URI, 'client_id':settings.CLIENT_ID, 'scope':'patients:summary:read'})
 
 def doctor(request):
-    code = request.GET.get('code', None)
-    return render(request, 'happybirthday/doctor.html', {'code':code})
+    error = request.GET.get('error')
+    if (error is not None):
+        #return render(request, 'happybirthday/error.html', {'message':error})
+        raise ValueError('Error authorizing application: %s' % error)
+
+    requestData = {
+        'code': request.GET.get('code'),
+        'grant_type': 'authorization_code',
+        'redirect_uri': settings.REDIRECT_URI,
+        'client_id': settings.CLIENT_ID,
+        'client_secret': settings.CLIENT_SECRET
+    }
+
+    response = requests.post('https://drchrono.com/o/token/', data=requestData)
+    response.raise_for_status()
+    data = response.json()
+
+    access_token = data['access_token']
+    refresh_token = data['refresh_token']
+    expires_timestamp = datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=data['expires_in'])
+    # save to db
+    return render(request, 'happybirthday/doctor.html', {})
