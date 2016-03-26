9c9a38a8f014383763e442e4d5d53ecb51554b75
cooltoast <mpatel678@gmail.com>
2016-03-25 17:09:10 -0700

    fetch patients as auth'd doctor

--- doctorsayshappybirthday/happybirthday/views.py
+++ doctorsayshappybirthday/happybirthday/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.conf import settings
 import datetime, pytz, requests
 
+BASE_URL = 'https://drchrono.com'
+
 # Create your views here.
 def login(request):
     return render(request, 'happybirthday/login.html', {'redirect_uri':settings.REDIRECT_URI, 'client_id':settings.CLIENT_ID, 'scope':'patients:summary:read'})
@@ -21,7 +23,7 @@ def doctor(request):
         'client_secret': settings.CLIENT_SECRET
     }
 
-    response = requests.post('https://drchrono.com/o/token/', data=requestData)
+    response = requests.post('%s/o/token/' % BASE_URL, data=requestData)
     response.raise_for_status()
     data = response.json()
 
@@ -29,4 +31,25 @@ def doctor(request):
     refresh_token = data['refresh_token']
     expires_timestamp = datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=data['expires_in'])
     # save to db
-    return render(request, 'happybirthday/doctor.html', {})
+
+    headers = {
+          'Authorization': 'Bearer %s' % access_token
+    }
+
+    response = requests.get('%s/api/users/current' % BASE_URL, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+
+    # save to db
+    doctor_id = data['id']
+    username = data['username']
+
+    patients = []
+    patients_url = '%s/api/patients_summary' % BASE_URL
+    while patients_url:
+      data = requests.get(patients_url, headers=headers).json()
+      patients.extend(data['results'])
+      patients_url = data['next'] # A JSON null on the last page
+
+    return render(request, 'happybirthday/doctor.html', {'patients':patients})
+
