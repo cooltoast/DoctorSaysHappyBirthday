262bae1fe72e12600dd298554217965223494e78
cooltoast <mpatel678@gmail.com>
2016-03-29 17:23:09 -0700

    setup email configs

--- doctorsayshappybirthday/doctorsayshappybirthday/settings.py
+++ doctorsayshappybirthday/doctorsayshappybirthday/settings.py
@@ -91,6 +91,15 @@ DATABASES = {
     }
 }
 
+# Email
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mpdrchrono'
+EMAIL_HOST_PASSWORD = keys['gmail_password']
+DEFAULT_FROM_EMAIL = 'mpdrchrono'
+DEFAULT_TO_EMAIL = 'to email'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
